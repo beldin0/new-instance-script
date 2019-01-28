ea7ab0ec393a0edbcee4bd88a8ac273582be0486
James Ritchie <james.g.ritchie@baesystems.com>
2019-01-28 10:33:07 +0000

    removed from script sudo. Execute script with sudo to setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
-sudo yum -y update
-sudo yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
-sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y install xrdp tigervnc-server
-sudo systemctl start xrdp
-sudo systemctl enable xrdp
-sudo firewall-cmd --permanent --add-port=3389/tcp
-sudo firewall-cmd --reload
-sudo chcon --type=bin_t /usr/sbin/xrdp
-sudo chcon --type=bin_t /usr/sbin/xrdp-sesman
+yum -y update
+yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
+ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install xrdp tigervnc-server
+systemctl start xrdp
+systemctl enable xrdp
+firewall-cmd --permanent --add-port=3389/tcp
+firewall-cmd --reload
+chcon --type=bin_t /usr/sbin/xrdp
+chcon --type=bin_t /usr/sbin/xrdp-sesman
 echo Enter your username
 read varname
-sudo useradd $varname
-sudo passwd $varname
-sudo usermod -aG wheel $varname
+useradd $varname
+passwd $varname
+usermod -aG wheel $varname
 
 echo "Do you wish to install VS Code? Choose option 1 or 2"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc; sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';yum check-update; sudo yum install -y code; break;;
+        Yes ) rpm --import https://packages.microsoft.com/keys/microsoft.asc; sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';yum check-update; yum install -y code; break;;
         No ) break;;
     esac
 done
@@ -26,7 +26,7 @@ done
 echo "Do you wish to install GoLang? Choose option 1 or 2"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo yum install -y golang; break;;
+        Yes ) yum install -y golang; break;;
         No ) break;;
     esac
 done
