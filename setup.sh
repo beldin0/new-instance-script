29982b6ca3d83cf1213cadad7176bd55d6338dd2
James Ritchie <james.g.ritchie@baesystems.com>
2019-01-28 09:39:09 +0000

    Added permissions 'wheel'

--- setup.sh
+++ setup.sh
@@ -15,15 +15,17 @@ sudo useradd $varname
 sudo passwd $varname
 
 echo "Do you wish to install VS Code?"
-select yn in "Yes" "No"; do
+select yn in "y" "n"; do
     case $yn in
-        Yes ) sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc; sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';yum check-update; sudo yum install -y code; break;;
+        y ) sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc; sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';yum check-update; sudo yum install -y code; break;;
+        n ) break;;
     esac
 done
 
 echo "Do you wish to install VS Code?"
-select yn in "Yes" "No"; do
+select yn in "y" "n"; do
     case $yn in
-        Yes ) sudo yum install -y golang; break;;
+        y ) sudo yum install -y golang; break;;
+        n ) break;;
     esac
 done
